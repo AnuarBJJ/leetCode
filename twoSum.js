5025b8b4ca67eae63f19baf1f366127e1b14450c
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-03-20 21:56:19 -0700

    initial commit

---
+++ twoSum.js
@@ -0,0 +1,13 @@
+var twoSum = function(nums, target) {
+    let runner = 0;
+    while(runner < nums.length){cd ..
+        const diff =  target-nums[runner];
+        const hasDiff = nums.indexOf(diff);
+        console.log(hasDiff)
+        if(hasDiff > -1){
+            return [runner, nums.indexOf(target-nums[runner]) ]
+        }
+        runner++
+    }
+    return 'The array has no solution with the target'
+};
