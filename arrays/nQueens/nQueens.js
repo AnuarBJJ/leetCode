2817fffbc6edb3250eec15a69479b8a1d8d0fa98
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-06-05 14:46:56 -0700

    n-queen solution

--- arrays/nQueens/nQueens.js
+++ arrays/nQueens/nQueens.js
@@ -22,3 +22,88 @@ There exist two distinct solutions to the 4-queens puzzle:
   ".Q.."]
 ]
 */
+
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+    const result = [];
+    const board = [];
+
+
+
+    for(let i=0; i<n; i++){
+      let t = [];
+      for(let j=0; j<n; j++){
+        t.push('.');
+      }
+      board.push(t);
+    }
+
+    const valid = function(r,c){
+        let row = r;
+        let col = c;
+        while(col>0){
+            col--;
+            if(board[r][col] === 'Q'){
+                return false;
+            }
+        }
+        while(row>0){
+            row--;
+            if(board[row][c]==='Q'){
+                return false;
+            }
+        }
+        row = r;
+        col = c;
+        while(row>0 && col>0){
+            row--;
+            col--;
+            if(board[row][col]==='Q'){
+                return false;
+            }
+        }
+        row = r;
+        col = c;
+        while(row>0 && col<n){
+            row--;
+            col++;
+            if(board[row][col]==='Q'){
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const putQueen = function(row){
+        if(row>n-1){
+            let solution = [];
+            board.forEach( (row) => {
+              let solRow = ''
+                row.forEach( p => {
+                    if(p!=='Q'){
+                        solRow += '.';
+                    }else {
+                        solRow += 'Q';
+                    }
+                });
+                solution.push(solRow);
+            });
+            return result.push(solution);
+        }
+        for(let col=0; col<n; col++){
+            if(valid(row, col)){
+                board[row][col] = 'Q';
+                putQueen(row+1);
+                board[row][col] = 'e';
+            }
+
+        }
+    };
+
+    putQueen(0);
+
+    return result;
+};
