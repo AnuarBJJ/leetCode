e94f176af95908af2623f9b0d86505dcef72b7c1
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-06-11 17:04:50 -0700

    smart solution to number of 1 bits

--- bitManipulation/bitManipulation.js
+++ bitManipulation/bitManipulation.js
@@ -20,3 +20,16 @@ var hammingWeight = function(n) {
     }
     return count;
 };
+
+// better solution
+/*
+var hammingWeight = function(n) {
+    var count = 0;
+    while (n != 0) {
+        n = n & (n - 1);
+        count++;
+    }
+
+    return count;
+};
+*/
