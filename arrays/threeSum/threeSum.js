7b8c0e35441698174db4c967743426bca3d24c4d
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-15 17:58:44 -0700

    three sum slow

---
+++ arrays/threeSum/threeSum.js
@@ -0,0 +1,59 @@
+/*
+Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+
+// slow solution
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+/*
+var twoSum = function(arr, target){
+  const numbers = {};
+  const res = [];
+
+  for(let n=0; n<arr.length; n++){
+    if( numbers[target-arr[n]] ){
+      res.push( [target-arr[n], arr[n]] );
+    } else {
+      numbers[arr[n]] = true;
+    }
+  }
+
+
+  return res;
+}
+
+var threeSum = function(nums) {
+    const miniArr = nums.slice();
+    const result = [];
+    const res = {};
+
+    for(let i=nums.length-1; i>=0; i--){
+      let popped = miniArr.pop();
+      let twoSumResult = twoSum( miniArr, - popped );
+
+      if( twoSumResult.length > 0 ){
+        twoSumResult.forEach( a => {
+            a[2] = popped;
+            result.push(a);
+        })
+      }
+    }
+
+  return result;
+};
+*/
