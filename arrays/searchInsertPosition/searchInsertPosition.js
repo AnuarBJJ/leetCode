60510974c30a9ce3c4b07dd7656a36efad53cc9b
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-19 15:59:24 -0700

    search insert position solution

--- arrays/searchInsertPosition/searchInsertPosition.js
+++ arrays/searchInsertPosition/searchInsertPosition.js
@@ -10,3 +10,40 @@ Here are few examples.
 [1,3,5,6], 7 → 4
 [1,3,5,6], 0 → 0
 */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let beg = 0;
+    let end = nums.length - 1;
+
+    while( beg <= end ){
+        let middle = parseInt((beg+end)/2);
+
+        if( nums[middle] === target ){
+          return middle;
+        } else if( nums[middle] > target ){
+            end = middle - 1;
+        } else {
+            beg = middle + 1;
+        }
+    }
+
+    return beg;
+};
+
+/*
+// for some reason 8-0 the following solution works faster. Even though its runtime is O(n) and previous is O(logN)
+
+var searchInsert = function(nums, target) {
+    for(let i = 0, end = nums.length; i < end; i++) {
+        if(nums[i] >= target) {
+            return i;
+        }
+    }
+
+    return nums.length;
+};
+*/
