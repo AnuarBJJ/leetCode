6dc849eace7ce43ae152776bf35af6119ddb88f0
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-22 16:21:22 -0700

    invert binary tree solution

--- binaryTree/invertBinaryTree/invertBinaryTree.js
+++ binaryTree/invertBinaryTree/invertBinaryTree.js
@@ -16,3 +16,40 @@ Trivia:
 This problem was inspired by this original tweet by Max Howell:
 Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so fuck off.
 */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(!root){
+        return root;
+    }
+    if(root.left && root.right){
+        let newNode = new TreeNode(0);
+        newNode.val = root.left.val
+        newNode.left = root.left.left;
+        newNode.right = root.left.right;
+
+        root.left = root.right;
+        root.right = newNode;
+
+        invertTree(root.left );
+        invertTree(root.right);
+    } else if( root.left ){
+        root.right = root.left;
+        root.left = null;
+        invertTree(root.right);
+    } else if( root.right ){
+        root.left = root.right;
+        root.right = null;
+        invertTree(root.left);
+    }
+    return root;
+};
