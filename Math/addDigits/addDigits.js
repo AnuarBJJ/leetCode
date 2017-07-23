91d051458ebb94ad131f4d89b65c0704df4da41c
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-22 19:00:45 -0700

    add digits solution

--- Math/addDigits/addDigits.js
+++ Math/addDigits/addDigits.js
@@ -5,3 +5,18 @@ For example:
 
 Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
 */
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    while(num>=10){
+        let interim = 0;
+        while(num!==0){
+            interim += num%10;
+            num = Math.floor(num/10);
+        }
+        num = interim;
+    }
+    return num;
+};
