a94f034875e9fd6f1780ae7ff835dcd7020805d4
AnuarBJJ <anuar.zhukenov@gmail.com>
2017-07-23 00:39:47 -0700

    climbing stairs solution

--- Math/climbingStaircase/climbingStaircase.js
+++ Math/climbingStaircase/climbingStaircase.js
@@ -5,3 +5,16 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 Note: Given n will be a positive integer.
 */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let res = [0, 1, 2];
+
+var climbStairs = function(n) {
+    if(n<res.length){
+        return res[n]
+    }
+    res[n] = climbStairs(n-2) + climbStairs(n-1);
+    return res[n];
+};
