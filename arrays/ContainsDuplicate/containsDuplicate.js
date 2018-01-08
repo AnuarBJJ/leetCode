e0165d519e703856b66706f08eb7331345666ac8
codalife <anuar.zhukenov@gmail.com>
2018-01-07 16:07:13 -0800

    contains duplicate

---
+++ arrays/ContainsDuplicate/containsDuplicate.js
@@ -0,0 +1,20 @@
+/**
+ * Given an array of integers, find if the array contains any duplicates.
+ * Your function should return true if any value appears at least twice in the array,
+ * and it should return false if every element is distinct.
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  const Seen = new Set();
+  let soFar = 0;
+
+  return nums.some(num => {
+    if (Seen.has(num)) {
+      return true;
+    } else {
+      Seen.add(num);
+      return false;
+    }
+  });
+};
