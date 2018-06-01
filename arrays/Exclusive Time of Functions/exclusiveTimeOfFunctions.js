875f15fc251756f2ded12c4317b680fbb90d953e
codalife <anuar.zhukenov@gmail.com>
2018-05-31 19:24:04 -0700

    exclusive time of functions

--- arrays/Exclusive Time of Functions/exclusiveTimeOfFunctions.js
+++ arrays/Exclusive Time of Functions/exclusiveTimeOfFunctions.js
@@ -1,9 +1,43 @@
-var exclusiveTime = function(n, logs) {
-  // keep a map of current times of all functions seen
-  // keep a stack of ongoing functions
+/*
+Given the running logs of n functions that are executed in a nonpreemptive single threaded CPU, find the exclusive time of these functions.
 
+Each function has a unique id, start from 0 to n-1. A function may be called recursively or by another function.
+
+A log is a string has this format : function_id:start_or_end:timestamp. For example, "0:start:0" means function 0 starts from the very beginning of time 0. "0:end:0" means function 0 ends to the very end of time 0.
+
+Exclusive time of a function is defined as the time spent within this function, the time spent by calling other functions should not be considered as this function's exclusive time. You should return the exclusive time of each function sorted by their function id.
+
+Example 1:
+Input:
+n = 2
+logs =
+["0:start:0",
+ "1:start:2",
+ "1:end:5",
+ "0:end:6"]
+Output:[3, 4]
+Explanation:
+Function 0 starts at time 0, then it executes 2 units of time and reaches the end of time 1.
+Now function 0 calls function 1, function 1 starts at time 2, executes 4 units of time and end at time 5.
+Function 0 is running again at time 6, and also end at the time 6, thus executes 1 unit of time.
+So function 0 totally execute 2 + 1 = 3 units of time, and function 1 totally execute 4 units of time.
+Note:
+Input logs will be sorted by timestamp, NOT log id.
+Your output should be sorted by function id, which means the 0th element of your output corresponds to the exclusive time of function 0.
+Two functions won't start or end at the same time.
+Functions could be called recursively, and will always end.
+1 <= n <= 100
+*/
+/**
+ * @param {number} n
+ * @param {string[]} logs
+ * @return {number[]}
+ */
+var exclusiveTime = function(n, logs) {
   const stack = [];
-  const funcs = new Map();
+  const funcs = new Array(n).fill(0);
+
+  let currentTime = 0;
 
   logs.forEach(log => {
     const data = log.split(':');
@@ -12,45 +46,18 @@ var exclusiveTime = function(n, logs) {
 
     if (data[1] === 'start') {
       if (stack.length) {
-        let parentFunc = stack[stack.length - 1];
-        parentFunc.span = +parentFunc.span + data[2] - parentFunc.start;
-      }
-      stack.push({
-        start: +data[2],
-        span: 0,
-        id: +data[0],
-      });
-    } else if (data[1] === 'end') {
-      const soFar = funcs.has(+data[0]) ? funcs.get(+data[0]) : 0;
-      const lastFunc = stack.pop();
-      const addOne = lastFunc.span === 0 ? 1 : 0;
-      const span = soFar + data[2] - lastFunc.start + lastFunc.span + addOne;
-      if (stack.length) {
         const parentFunc = stack[stack.length - 1];
-        parentFunc.start = +data[2];
+        funcs[parentFunc] += data[2] - currentTime;
       }
-      funcs.set(data[0], span);
+      stack.push(data[0]);
+      currentTime = data[2];
+    } else if (data[1] === 'end') {
+      const lastFunc = stack.pop();
+      funcs[lastFunc] += data[2] - currentTime + 1;
+
+      currentTime = data[2] + 1;
     }
   });
 
-  const result = [];
-
-  for (let func of funcs) {
-    result[+func[0]] = func[1];
-  }
-
-  return result;
+  return funcs;
 };
-
-const testLogs = [
-  '0:start:0',
-  '0:start:2',
-  '0:end:5',
-  '0:start:6',
-  '0:end:6',
-  '0:end:7',
-];
-
-const testResult = exclusiveTime(1, testLogs);
-
-console.log(testResult);
